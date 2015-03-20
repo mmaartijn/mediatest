8682ce06949fe2a848c455aae1cdd8ba0cd67909
mmaartijn <mmaartijn@gmail.com>
2015-03-20 15:18:41 +0100

    Media upload

---
+++ Start server.cmd
@@ -0,0 +1,2 @@
+"C:\Program Files\Python 3.5\python.exe" -m http.server
+pause
--- www/js/index.js
+++ www/js/index.js
@@ -1,19 +1,16 @@
 var media;
+var secondsRecorded = 0;
+var maxSeconds = 5;
+var interval;
+var lastState;
+var filename = 'myRecording.mp3';
+var uploadURL = 'http://shinefestival.herokuapp.com';
 
-function createMedia(){
-    return new Media('myFile.mp3', 
-        function(){ 
-            //alert('success!');
-        }, function(err){
-            alert(err.message); 
-        }
-    );
-};
+document.addEventListener('deviceready', function(){
+    updateCurrentState('idle');
+    updateSecondsRecordedUI();
 
-$(document).ready(function(){
     $('button').on('tap', function(e){
-        $('#txtLastButton').text($(this).text());
-        
         switch ($(this).attr('id'))
         {
             case 'btnStart':
@@ -25,24 +22,55 @@ $(document).ready(function(){
             case 'btnPlay':
                 playRecordedFile();
                 break;
-            case 'btnStopPlay':
-                stopRecordedFile();
+            case 'btnSend':
+                sendRecordedFile();
                 break;
         }
     });
 });
 
 function startRecording(){
-    $('#btnStop').prop('disabled', false);
-    $('#btnStart').prop('disabled', true);
+    updateCurrentState('recording');
     media = createMedia();
     media.startRecord();
+    interval = setInterval(function(){
+        secondsRecorded++;
+        updateSecondsRecordedUI();
+
+        if(secondsRecorded >= maxSeconds){
+            stopRecording();
+        }
+    }, 1000);
+}
+
+function createMedia(){
+    return new Media(filename, 
+        function(){
+            if(lastState == 'playing'){
+                updateCurrentState('recorded');
+            }
+        }, 
+        function(err){
+            alert(err.message); 
+        }
+    );
+};
+
+function updateSecondsRecordedUI(){
+    var secondsLeft = maxSeconds - secondsRecorded;
+    var text = (secondsLeft < 10 ? '0' : '') + secondsLeft;
+    $('#textSecondsLeft').html('00:' + text);
 }
 
 function stopRecording(){
-    $('#btnStop').prop('disabled', true);
-    $('#btnStart').prop('disabled', false);
-    $('#btnPlay').prop('disabled', false);
+    if(interval){
+        clearInterval(interval);
+    }
+
+    updateCurrentState('recorded');
+
+    secondsRecorded = 0;
+    updateSecondsRecordedUI();
 
     if(media){
         media.stopRecord();
@@ -52,18 +80,60 @@ function stopRecording(){
 }
 
 function playRecordedFile(){
-    $('#btnStart').prop('disabled', true);
-    $('#btnPlay').prop('disabled', true);
-    $('#btnStopPlay').prop('disabled', false);
+    updateCurrentState('playing');
     media = createMedia();
     media.play();
 }
 
-function stopRecordedFile(){
-    $('#btnStopPlay').prop('disabled', true);
-    $('#btnStart').prop('disabled', false);
-    $('#btnPlay').prop('disabled', false);
+function sendRecordedFile(){
+    updateCurrentState('idle');
+    $('#textSendStatus').html('uploading...');
 
-    media.pause();
-    media.release();
+    window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, function (fileSystem) {
+        fileSystem.root.getFile(filename, { create: false, exclusive: false }, function(fileEntry){
+            var options = new FileUploadOptions();
+            options.fileKey = "recordedAudio";
+            options.fileName = filename;
+            options.mimeType = 'audio/mpeg';
+            options.chunkedMode = false;
+
+            var ft = new FileTransfer();
+            ft.upload(fileEntry.toURL(), uploadURL, 
+                function(res){
+                    $('#textSendStatus').html('success!');
+                }, function(err){
+                    alert('oh no!');
+                    $('#textSendStatus').html(err.body);
+                }, options);
+        });
+    });
+
+    
+}
+
+function updateCurrentState(status){
+    lastState = status;
+    switch (status){
+        case 'idle':
+            $('#btnStart').prop('disabled', false);
+            $('#btnStop').prop('disabled', true);
+            $('#btnPlay').prop('disabled', true);
+            $('#btnSend').prop('disabled', true);
+            break;
+        case 'recorded':
+            $('#btnStart').prop('disabled', false);
+            $('#btnPlay').prop('disabled', false);
+            $('#btnSend').prop('disabled', false);
+            break;
+        case 'recording':
+            $('#btnStop').prop('disabled', false);
+            $('#btnStart').prop('disabled', true);
+            $('#btnSend').prop('disabled', true);
+            break;
+        case 'playing':
+            $('#btnStart').prop('disabled', true);
+            $('#btnPlay').prop('disabled', true);
+            $('#btnSend').prop('disabled', true);
+            break;
+    }
 }
